285889665e287b6445455dfb3baa521c4aabe0ae
ABHINASH SAHOO <abinashsahoo626@gmail.com>
2026-05-25 20:32:46 +0530

    Class Lerning
    revision about java 25th may 2026
    time 12:20pm

---
+++ com/Abstract_Day2/AbstractDemo.java
@@ -0,0 +1,44 @@
+package com.Abstract_Day2;
+
+abstract class Sim{
+    abstract void call();
+    abstract void message();
+}
+class Airtel extends Sim{
+    @Override
+    void call(){
+        System.out.println("Calling using Airtel");
+    }
+    @Override
+    void message(){
+        System.out.println("Messaging using Airtel");
+    }
+}
+class Jio extends Sim{
+    @Override
+    void call(){
+        System.out.println("Calling using Jio");
+    }
+    @Override
+    void message(){
+        System.out.println("Messaging using Jio");
+    }
+}
+class Phone{
+    void useCall(Sim sim){
+        sim.call();
+    }
+    void useMessage(Sim sim){
+        sim.message();
+    }
+}
+public class AbstractDemo {
+    public static void main(String[] args) {
+        Sim sim1 = new Airtel();
+        Sim sim2 = new Jio();
+        Phone phone = new Phone();
+        phone.useCall(sim1);
+        phone.useMessage(sim2);
+        
+    }
+}
---
+++ com/Abstract_Day2/InterfaceDemo.java
@@ -0,0 +1,49 @@
+package com.Abstract_Day2;
+
+interface Mobile {
+    void call();
+    void message();
+    default void internet(){
+        System.out.println("Using Internet");
+    }
+    // private void Security(){
+    //     System.out.println("Ensuring Security");
+    // }
+    // static void music(){
+    //     System.out.println("Listening to Music");
+    // }
+
+}
+class Vivo implements Mobile{
+    @Override
+    public void call(){
+        System.out.println("Calling using Vivo");
+    }
+    @Override
+    public void message(){
+        System.out.println("Messaging using Vivo");
+    }
+    @Override
+    public void internet(){
+        System.out.println("Using Internet on Vivo");
+    }
+    // @Override
+    // public void Security(){
+    //     System.out.println("Ensuring Security on Vivo");
+    // }
+    // @Override
+    // public void music(){
+    //     System.out.println("Listening to Music on Vivo");
+    // }
+}
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Mobile mobile = new Vivo();
+        mobile.call();
+        mobile.message();
+        mobile.internet();
+        // mobile.Security();
+        // mobile.music();
+    }
+}
