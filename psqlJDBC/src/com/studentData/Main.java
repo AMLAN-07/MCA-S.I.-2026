ade281ea46e246005a475980dc3b5eff0ecbed94
ABHINASH SAHOO <abinashsahoo626@gmail.com>
2026-05-30 12:32:03 +0530

    JDBC
    PostgreSQL JDBC
    Create a basic CURD operation
    30th MAY 2026 [12:31pm]

---
+++ psqlJDBC/src/com/studentData/Main.java
@@ -0,0 +1,78 @@
+package com.studentData;
+
+import java.util.Scanner;
+
+import com.studentData.StudentCURD.StudentDAO;
+import com.studentData.DAO.Student;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        StudentDAO dao = new StudentDAO();
+        
+        
+
+        while (true) {
+
+            System.out.println("\n===== STUDENT MANAGEMENT =====");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Update Student");
+            System.out.println("4. Delete Student");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter Choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1->{
+                    System.out.print("Enter ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Enter Course: ");
+                    String course = sc.nextLine();
+
+                    dao.addStudent(new Student(id, name, course));
+                }
+
+                case 2->{
+                    dao.getAllStudents();
+                }
+
+                case 3->{
+                    System.out.print("Enter ID: ");
+                    int updateId = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Enter New Course: ");
+                    String newCourse = sc.nextLine();
+
+                    dao.updateStudent(new Student(updateId, null, newCourse));
+                }
+
+                case 4->{
+                    System.out.print("Enter ID: ");
+                    int deleteId = sc.nextInt();
+
+                    dao.deleteStudent(new Student(deleteId, null, null));
+                }
+
+                case 5->{
+                    System.out.println("Thank You! visit again.");
+                    sc.close();
+                    System.exit(0);}
+
+                default->{
+                    System.out.println("Invalid Choice!");}
+            }
+        }
+    }
+}
---
+++ psqlJDBC/src/com/studentData/StudentCURD/StudentDAO.java
@@ -0,0 +1,113 @@
+package com.studentData.StudentCURD;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.studentData.Database.DBConnection;
+import com.studentData.DAO.Student;
+
+public class StudentDAO {
+
+    public void addStudent(Student student) {
+
+        try {
+            Connection con = DBConnection.getConnection();
+
+            String sql = "INSERT INTO student(id, name, course) VALUES (?, ?, ?)";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setInt(1, student.getId());
+            ps.setString(2, student.getName());
+            ps.setString(3, student.getCourse());
+
+            int rows = ps.executeUpdate();
+
+            if (rows > 0) {
+                System.out.println("Student Added Successfully!");
+            }
+
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void getAllStudents() {
+
+    try {
+        Connection con = DBConnection.getConnection();
+
+        String sql = "SELECT * FROM student";
+
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            System.out.println(
+                    rs.getInt("id") + " " +
+                    rs.getString("name") + " " +
+                    rs.getString("course")
+            );
+        }
+
+        con.close();
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+    public void updateStudent(Student student) {
+
+    try {
+        Connection con = DBConnection.getConnection();
+
+        String sql = "UPDATE student SET course = ? WHERE id = ?";
+
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        ps.setString(1, student.getCourse());
+        ps.setInt(2, student.getId());
+
+        int rows = ps.executeUpdate();
+
+        if (rows > 0) {
+            System.out.println("Student Updated Successfully!");
+        } else {
+            System.out.println("Student Not Found!");
+        }
+
+        con.close();
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+    public void deleteStudent(Student student) {
+
+    try {
+        Connection con = DBConnection.getConnection();
+
+        String sql = "DELETE FROM student WHERE id = ?";
+
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        ps.setInt(1, student.getId());
+
+        int rows = ps.executeUpdate();
+
+        if (rows > 0) {
+            System.out.println("Student Deleted Successfully!");
+        } else {
+            System.out.println("Student Not Found!");
+        }
+
+        con.close();
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+}
